d7d8b3c4bb8e4544b4505a04cb2078403b4b7107
Prashant Mehta <prashantmehta@Prashants-MacBook-Air.local>
2023-12-04 17:50:26 +0530

    code is running now

--- src/main/java/com/Prashant/WaterInventory/config/EmailConfigure.java
+++ src/main/java/com/Prashant/WaterInventory/config/EmailConfigure.java
@@ -1,11 +1,9 @@
 package com.Prashant.WaterInventory.config;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -28,14 +26,14 @@ public class EmailConfigure {
 			MimeMessage message = javaMailSender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-			FileSystemResource resource = new FileSystemResource(new File(
-					"/Users/prashantmehta/Documents/workspace-spring-tool-suite-4-4.15.1.RELEASE/WaterInventory/src/main/resources/inventory.pdf"));
+//			FileSystemResource resource = new FileSystemResource(new File(
+//					"/Users/prashantmehta/Documents/workspace-spring-tool-suite-4-4.15.1.RELEASE/WaterInventory/src/main/resources/inventory.pdf"));
 
 			helper.setTo(to);
 			helper.setSubject(subject);
 			helper.setFrom(username);
 			helper.setText("hello prashant , your Inventory invoice here");
-			helper.addAttachment(resource.getFilename(), resource, "application/pdf");
+//			helper.addAttachment(resource.getFilename(), resource, "application/pdf");
 
 			javaMailSender.send(message);
 
