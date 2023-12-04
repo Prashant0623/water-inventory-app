bc1044c49ef8ab419270c290b78e1a6cdece5b02
Prashant Mehta <prashantmehta@Prashants-MacBook-Air.local>
2023-12-04 15:33:45 +0530

    new data added

--- src/main/java/com/Prashant/WaterInventory/repository/WaterRepo.java
+++ src/main/java/com/Prashant/WaterInventory/repository/WaterRepo.java
@@ -13,16 +13,16 @@ import com.Prashant.WaterInventory.entity.WaterModel;
 @Repository
 public interface WaterRepo extends JpaRepository<WaterModel, Long> {
 
-	@Query(value = "SELECT* FROM waterinventory.water_model where vendor_id=:id", nativeQuery = true)
+	@Query(value = "SELECT* FROM water_model where vendor_id=:id", nativeQuery = true)
 	public List<WaterModel> findByVendorId(Long id);
 
-	@Query(value = "SELECT* FROM  waterinventory.water_model where date=:date", nativeQuery = true)
+	@Query(value = "SELECT* FROM water_model where date=:date", nativeQuery = true)
 	public List<WaterModel> findByDate(String date);
 
-	@Query(value = "SELECT* FROM  waterinventory.water_model where date=:date AND vendor_id=:id", nativeQuery = true)
+	@Query(value = "SELECT* FROM water_model where date=:date AND vendor_id=:id", nativeQuery = true)
 	public List<WaterModel> findByDateAndVendorId(String date, Long id);
 
-	@Query(value = "SELECT * FROM waterinventory.water_model f order by f.id desc ", nativeQuery = true)
+	@Query(value = "SELECT * FROM water_model f order by f.id desc ", nativeQuery = true)
 	public Page<WaterModel> findAllOrderedByIdDesc(Pageable pageable);
 
 }
